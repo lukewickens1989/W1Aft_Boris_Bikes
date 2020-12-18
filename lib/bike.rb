9ac324e964586496b6d5f0114d46c5d0842fa79b
Luke Wickens <lukewickens1989@gmail.com>
2020-12-18 10:36:45 +0000

    Test drove Bike class set up with working? method and status variable.

---
+++ lib/bike.rb
@@ -0,0 +1,13 @@
+class Bike
+
+  attr_reader :status
+  
+  def initialize
+    @status = true
+  end
+
+  def working?
+
+  end
+
+end
---
+++ spec/bike_spec.rb
@@ -0,0 +1,22 @@
+require "bike"
+
+RSpec.describe Bike do
+  let(:bike) { described_class.new }
+
+  describe "initialization of class" do
+    it "should be instance of Bike class" do
+      expect(bike).to be_instance_of(Bike)
+    end
+  end
+
+  describe "#working?" do
+    it "a new instance of Bike class should respond to working?" do
+      expect(bike).to respond_to(:working?)
+    end
+
+    it "a new instance of bike class should start off working" do
+      expect(bike.status).to eq(true)
+    end
+  end
+
+end
